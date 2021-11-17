27cb627090dc70c8138cabf6a524fed70d5f25c2
Tyler Nijmeh <tylernij@gmail.com>
2021-11-17 15:55:04 -0800

    Upload code!
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # monolith
 Recompile Android packages with more extensive DEX optimizations
+
+# How
+1) Push script to device: `adb push monolith.sd /sdcard/Download`
+2) Using ADB shell or root, run the script: `sh /sdcard/Download/monolith.sh`
---
+++ monolith.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+
+MODE="everything"
+
+packages="$(pm list packages -3 | toybox sed 's/.*://')"
+for package in $packages
+do
+    echo "[1/3] $package"
+    pm compile -f --check-prof false -m "$MODE" "$package"
+    echo "[2/3] $package"
+    pm compile -f --check-prof false --secondary-dex -m "$MODE" "$package"
+    echo "[3/3] $package"
+    pm compile -f --check-prof false --compile-layouts "$package"
+    echo
+done
