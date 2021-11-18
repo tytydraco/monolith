ac024ed129d48132642c57e7be37b8629a3c55d7
Tyler <tylernij@gmail.com>
2021-11-17 18:11:31 -0800

    Update monolith.sh

--- monolith.sh
+++ monolith.sh
@@ -8,3 +8,8 @@ pm compile -a -f --check-prof false -m "$MODE"
 echo ">>> PART 2"
 echo
 pm compile -a -f --check-prof false --compile-layouts
+
+echo ">>> Cleaning up"
+pm bg-dexopt-job
+
+echo ">>> Done"
