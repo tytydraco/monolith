4017aae083635d559552837849a4e6041b1ce7c7
Tyler <tylernij@gmail.com>
2021-11-17 17:14:19 -0800

    Better way to do all packages

--- monolith.sh
+++ monolith.sh
@@ -2,14 +2,12 @@
 
 MODE="everything"
 
-packages="$(pm list packages | toybox sed 's/.*://')"
-for package in $packages
-do
-    echo "[1/3] $package"
-    pm compile -f --check-prof false -m "$MODE" "$package"
-    echo "[2/3] $package"
-    pm compile -f --check-prof false --secondary-dex -m "$MODE" "$package"
-    echo "[3/3] $package"
-    pm compile -f --check-prof false --compile-layouts "$package"
-    echo
-done
+echo ">>> PART 1"
+echo
+pm compile -a -f --check-prof false -m "$MODE"
+echo ">>> PART 2"
+echo
+pm compile -a -f --check-prof false --secondary-dex -m "$MODE"
+echo ">>> PART 3"
+echo
+pm compile -a -f --check-prof false --compile-layouts
