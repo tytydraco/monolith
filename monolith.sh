07b080ac8a2c3d481e39d23ee7af822f752dad1b
Tyler <tylernij@gmail.com>
2021-11-17 17:56:59 -0800

    Update monolith.sh

--- monolith.sh
+++ monolith.sh
@@ -7,7 +7,4 @@ echo
 pm compile -a -f --check-prof false -m "$MODE"
 echo ">>> PART 2"
 echo
-pm compile -a -f --check-prof false --secondary-dex -m "$MODE"
-echo ">>> PART 3"
-echo
 pm compile -a -f --check-prof false --compile-layouts
