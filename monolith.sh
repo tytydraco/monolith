034eda05785dbb3d6d2dec30e1b428176aee1ed2
Tyler Nijmeh <tylernij@gmail.com>
2022-05-21 13:58:15 -0700

    Update monolith.sh

--- monolith.sh
+++ monolith.sh
@@ -4,12 +4,13 @@ MODE="everything-profile"
 
 echo ">>> PART 1"
 echo
-pm compile -a -f -m "$MODE"
+cmd package compile -a -f -m "$MODE"
+
 echo ">>> PART 2"
 echo
-pm compile -a -f --compile-layouts
+cmd package compile -a -f --compile-layouts
 
 echo ">>> Cleaning up"
-pm bg-dexopt-job
+cmd package bg-dexopt-job
 
 echo ">>> Done"
