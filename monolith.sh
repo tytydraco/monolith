4898fae42609e39cc74f7aa67421c903a9032c10
Adithya S Sekhar <adithya0332@gmail.com>
2022-12-27 10:49:55 +0530

    Don't -f (force) as it's profile based and we'll waste time ignoring the profile changes in future recompilations.

--- monolith.sh
+++ monolith.sh
@@ -4,11 +4,11 @@ MODE="everything-profile"
 
 echo ">>> PART 1"
 echo
-cmd package compile -a -f -m "$MODE"
+cmd package compile -a -m "$MODE"
 
 echo ">>> PART 2"
 echo
-cmd package compile -a -f --compile-layouts
+cmd package compile -a --compile-layouts
 
 echo ">>> Cleaning up"
 cmd package bg-dexopt-job
